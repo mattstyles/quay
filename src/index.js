be78593a1363b9c45bbd1f4ab625ba7d4867d51f
mattstyles <matt@veryfizzyjelly.com>
2015-11-25 09:11:46 +0000

    #update linting

--- src/index.js
+++ src/index.js
@@ -12,226 +12,225 @@ var pressed = new Map()
  * Quay emits stream events on keydown events
  */
 export default class Quay {
-    /**
-     * @constructs
-     * @param el <Element> _window_ element to attach to
-     */
-    constructor( el = window ) {
-        this.el = el
-        this.raf = raf( this.el, this._onTick )
-        this.raf.pause()
-        this.keys = new Map()
+  /**
+   * @constructs
+   * @param el <Element> _window_ element to attach to
+   */
+  constructor( el = window ) {
+    this.el = el
+    this.raf = raf( this.el, this._onTick )
+    this.raf.pause()
+    this.keys = new Map()
 
-        this.el.addEventListener( 'focus', this._attach )
-        this.el.addEventListener( 'blur', this._detach )
+    this.el.addEventListener( 'focus', this._attach )
+    this.el.addEventListener( 'blur', this._detach )
 
-        this._attach()
+    this._attach()
+  }
+
+  /**
+   * @public
+   * Allow access to the static class member, no need to privatise it as its
+   * handy for consumers to implement key combos
+   */
+  get pressed() {
+    return pressed
+  }
+
+  /**
+   * @private
+   * Attaches key listeners
+   */
+  _attach = () => {
+    this.el.addEventListener( 'keydown', this._onKeydown )
+    this.el.addEventListener( 'keyup', this._onKeyup )
+  }
+
+  /**
+   * @private
+   * Detaches key listeners and clears any currently pressed keys
+   */
+  _detach = () => {
+    this.el.removeEventListener( 'keydown', this._onKeydown )
+    this.el.removeEventListener( 'keyup', this._onKeyup )
+
+    pressed.clear()
+  }
+
+  /**
+   * @private
+   * Adds keypresses to the active list
+   */
+  _onKeydown = ( event ) => {
+    let key = vkey[ event.keyCode ]
+
+    // Bail on repeated keypresses
+    if ( pressed.has( key ) ) {
+      return
     }
 
-    /**
-     * @public
-     * Allow access to the static class member, no need to privatise it as its
-     * handy for consumers to implement key combos
-     */
-    get pressed() {
-        return pressed
+    pressed.set( key, event )
+
+    let stream = this.keys.get( key )
+    if ( stream ) {
+      stream.emit( 'keydown', event )
     }
 
-    /**
-     * @private
-     * Attaches key listeners
-     */
-    _attach = () => {
-        this.el.addEventListener( 'keydown', this._onKeydown )
-        this.el.addEventListener( 'keyup', this._onKeyup )
+    // Emit events while keys are pressed
+    this._resume()
+  }
+
+  /**
+   * @private
+   * Removes keypresses from the list of active keys
+   */
+  _onKeyup = ( event ) => {
+    let key = vkey[ event.keyCode ]
+
+    // In theory, this should never occur
+    if ( !pressed.has( key ) ) {
+      // @TODO error handle here as this should be impossible
+      return
     }
 
-    /**
-     * @private
-     * Detaches key listeners and clears any currently pressed keys
-     */
-    _detach = () => {
-        this.el.removeEventListener( 'keydown', this._onKeydown )
-        this.el.removeEventListener( 'keyup', this._onKeyup )
-
-        pressed.clear()
+    let stream = this.keys.get( key )
+    if ( stream ) {
+      stream.emit( 'keyup', event )
     }
 
-    /**
-     * @private
-     * Adds keypresses to the active list
-     */
-    _onKeydown = ( event ) => {
-        let key = vkey[ event.keyCode ]
+    pressed.delete( key )
 
-        // Bail on repeated keypresses
-        if ( pressed.has( key ) ) {
-            return
-        }
+    // With no keypresses stop spamming the tick
+    if ( pressed.size <= 0 ) {
+      this._pause()
+    }
+  }
 
-        pressed.set( key, event )
-
-        let stream = this.keys.get( key )
-        if ( stream ) {
-            stream.emit( 'keydown', event )
-        }
-
-        // Emit events while keys are pressed
-        this._resume()
+  /**
+   * @private
+   * Start spamming out key press events
+   */
+  _resume() {
+    if ( !this.raf.paused ) {
+      return
     }
 
-    /**
-     * @private
-     * Removes keypresses from the list of active keys
-     */
-    _onKeyup = ( event ) => {
-        let key = vkey[ event.keyCode ]
+    this.raf.resume()
+  }
 
-        // In theory, this should never occur
-        if ( !pressed.has( key ) ) {
-            // @TODO error handle here as this should be impossible
-            return
-        }
-
-        let stream = this.keys.get( key )
-        if ( stream ) {
-            stream.emit( 'keyup', event )
-        }
-
-        pressed.delete( key )
-
-        // With no keypresses stop spamming the tick
-        if ( pressed.size <= 0 ) {
-            this._pause()
-        }
+  /**
+   * @private
+   * Stop spamming key events to streams
+   */
+  _pause() {
+    if ( this.raf.paused ) {
+      return
     }
 
-    /**
-     * @private
-     * Start spamming out key press events
-     */
-    _resume() {
-        if ( !this.raf.paused ) {
-            return
-        }
+    this.raf.pause()
+  }
 
-        this.raf.resume()
+  /**
+   * @private
+   * Iterates through active keys and emits key events
+   */
+  _onTick = ( delta ) => {
+    pressed.forEach( ( val, key ) => {
+      let stream = this.keys.get( key )
+      if ( !stream ) {
+        return
+      }
+
+      // Custom emitter, sanitizes keypress
+      stream.fire({
+        raw: val,
+        delta: delta
+      })
+    }, this )
+  }
+
+
+  /*-----------------------------------------------------------*
+   *
+   *  Public API
+   *
+   *-----------------------------------------------------------*/
+
+  /**
+   * @public
+   * @param key <String> vkey key identifier
+   * @param cb <Function> _optional_
+   * @returns <EventEmitter> event stream
+   * Adds an event stream listener. Stream emits events when the key is pressed
+   */
+  stream( key, cb = null ) {
+    // If key stream already exist then bail
+    if ( this.keys.has( key ) ) {
+      // @TODO consider multiple streams to one key, but its probably best
+      // this is handled by the caller and not the lib
+      return false
     }
 
-    /**
-     * @private
-     * Stop spamming key events to streams
-     */
-    _pause() {
-        if ( this.raf.paused ) {
-            return
-        }
+    let emitter = new KeyStream()
 
-        this.raf.pause()
+    if ( cb ) {
+      emitter.on( 'data', cb )
     }
 
-    /**
-     * @private
-     * Iterates through active keys and emits key events
-     */
-    _onTick = ( delta ) => {
-        pressed.forEach( ( val, key ) => {
-            let stream = this.keys.get( key )
-            if ( !stream ) {
-                return
-            }
+    this.keys.set( key, emitter )
+    return emitter
+  }
 
-            // Custom emitter, sanitizes keypress
-            stream.fire({
-                raw: val,
-                delta: delta
-            })
-        }, this )
+  /**
+   * @public
+   * @param key <String> vkey key id
+   * @returns <Boolean>
+   */
+  removeStream( key ) {
+    if ( !this.keys.has( key ) ) {
+      return false
     }
 
+    this.keys.get( key ).destroy()
 
-    /*-----------------------------------------------------------*
-     *
-     *  Public API
-     *
-     *-----------------------------------------------------------*/
+    delete this.keys.get( key )
+    this.keys.delete( key )
 
-    /**
-     * @public
-     * @param key <String> vkey key identifier
-     * @param cb <Function> _optional_
-     * @returns <EventEmitter> event stream
-     * Adds an event stream listener. Stream emits events when the key is pressed
-     */
-    stream( key, cb = null ) {
-        // If key stream already exist then bail
-        if ( this.keys.has( key ) ) {
-            // @TODO consider multiple streams to one key, but its probably best
-            // this is handled by the caller and not the lib
-            return false
-        }
+    return true
+  }
 
-        let emitter = new KeyStream()
+  /**
+   * @public
+   * @param key <String> vkey key identifier
+   * @param cb <Function>
+   * @returns <EventEmitter> event stream
+   * Alias for `this.stream`
+   */
+  on( key, cb ) {
+    return this.stream( key, cb )
+  }
 
-        if ( cb ) {
-            emitter.on( 'data', cb )
-        }
+  /**
+   * @public
+   * @param key <String> vkey key id
+   * @returns <Boolean>
+   * Alias for removeStream
+   */
+  off( key ) {
+    return this.removeStream( key )
+  }
 
-        this.keys.set( key, emitter )
-        return emitter
-    }
-
-    /**
-     * @public
-     * @param key <String> vkey key id
-     * @returns <Boolean>
-     */
-    removeStream( key ) {
-        if ( !this.keys.has( key ) ) {
-            return false
-        }
-
-        this.keys.get( key ).destroy()
-
-        delete this.keys.get( key )
-        this.keys.delete( key )
-
-        return true
-    }
-
-    /**
-     * @public
-     * @param key <String> vkey key identifier
-     * @param cb <Function>
-     * @returns <EventEmitter> event stream
-     * Alias for `this.stream`
-     */
-    on( key, cb ) {
-        return this.stream( key, cb )
-    }
-
-    /**
-     * @public
-     * @param key <String> vkey key id
-     * @returns <Boolean>
-     * Alias for removeStream
-     */
-    off( key ) {
-        return this.removeStream( key )
-    }
-
-    /**
-     * @public
-     * @param key <String> vkey key identifier
-     * @param cb <Function>
-     * @returns <EventEmitter> event stream
-     * Fires just one event on first keypress
-     */
-    once( key, cb ) {
-        return this.stream( key, event => {
-            cb( event )
-            this.off( key )
-        })
-    }
-
+  /**
+   * @public
+   * @param key <String> vkey key identifier
+   * @param cb <Function>
+   * @returns <EventEmitter> event stream
+   * Fires just one event on first keypress
+   */
+  once( key, cb ) {
+    return this.stream( key, event => {
+      cb( event )
+      this.off( key )
+    })
+  }
 }
--- src/keystream.js
+++ src/keystream.js
@@ -6,86 +6,85 @@ import EventEmitter from 'eventemitter3'
  * Basic event emitter with some added extra info for listeners
  */
 export default class KeyStream extends EventEmitter {
-    constructor() {
-        super()
-
-        /**
-         * Start of this keypress
-         * @type <TimeStamp>
-         */
-        this.startTime = null
-
-        /**
-         * Start of the last keypress, useful for double taps or timers
-         * @type <TimeStamp>
-         */
-        this.lastDown = null
-
-        /**
-         * Duration of this keypress
-         * @type <Integer> in ms
-         */
-        this.delta = 0
-
-
-        this.on( 'keydown', this.onKeydown )
-        this.on( 'keyup', this.onKeyup )
-    }
+  constructor() {
+    super()
 
     /**
-     * Emits destroy event
+     * Start of this keypress
+     * @type <TimeStamp>
      */
-    destroy() {
-        this.off( 'keydown', this.onKeydown )
-        this.off( 'keyup', this.onKeyup )
-
-        this.emit( 'destroy' )
-    }
+    this.startTime = null
 
     /**
-     * Emits an event
-     * Santizes the raw key event
+     * Start of the last keypress, useful for double taps or timers
+     * @type <TimeStamp>
      */
-    fire = ( event ) => {
-        // This is necessary as the first `event.delta` will be from the animation
-        // frame and will be since the last 'tick' that was fired, after that every
-        // tick will fire and the delta will be correct
-        this.delta += this.delta === 0
-            ? 1
-            : event.delta
-
-        // Emit a data event with the timing info appended
-        this.emit( 'data', {
-            /**
-             * Measures from the last keypress to this keypress
-             */
-            since: event.raw.timeStamp - this.lastDown,
-
-            /**
-             * Measures the duration of this keypress
-             */
-            delta: this.delta
-        })
-    }
+    this.lastDown = null
 
     /**
-     * Fired on initial keydown event
-     * @param event <Object> passed from Quay, which should be the raw keyboard event
+     * Duration of this keypress
+     * @type <Integer> in ms
      */
-    onKeydown = ( event ) => {
-        this.startTime = event.timeStamp
-        this.delta = 0
+    this.delta = 0
 
-        if ( !this.lastDown ) {
-            this.lastDown = event.timeStamp
-        }
-    }
+    this.on( 'keydown', this.onKeydown )
+    this.on( 'keyup', this.onKeyup )
+  }
 
-    /**
-     * Fired on the keyup associated with this keypress
-     * @param event <Object> passed from Quay, which should be the raw keyboard event
-     */
-    onKeyup = ( event ) => {
-        this.lastDown = event.timeStamp
+  /**
+   * Emits destroy event
+   */
+  destroy() {
+    this.off( 'keydown', this.onKeydown )
+    this.off( 'keyup', this.onKeyup )
+
+    this.emit( 'destroy' )
+  }
+
+  /**
+   * Emits an event
+   * Santizes the raw key event
+   */
+  fire = ( event ) => {
+    // This is necessary as the first `event.delta` will be from the animation
+    // frame and will be since the last 'tick' that was fired, after that every
+    // tick will fire and the delta will be correct
+    this.delta += this.delta === 0
+      ? 1
+      : event.delta
+
+    // Emit a data event with the timing info appended
+    this.emit( 'data', {
+      /**
+       * Measures from the last keypress to this keypress
+       */
+      since: event.raw.timeStamp - this.lastDown,
+
+      /**
+       * Measures the duration of this keypress
+       */
+      delta: this.delta
+    })
+  }
+
+  /**
+   * Fired on initial keydown event
+   * @param event <Object> passed from Quay, which should be the raw keyboard event
+   */
+  onKeydown = ( event ) => {
+    this.startTime = event.timeStamp
+    this.delta = 0
+
+    if ( !this.lastDown ) {
+      this.lastDown = event.timeStamp
     }
+  }
+
+  /**
+   * Fired on the keyup associated with this keypress
+   * @param event <Object> passed from Quay, which should be the raw keyboard event
+   */
+  onKeyup = ( event ) => {
+    this.lastDown = event.timeStamp
+  }
 }
